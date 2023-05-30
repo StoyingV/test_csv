d03512c0ea88d19e418ce02e5550886e136bb87b
Vitaly Portnov <vitport@hotmail.com>
2023-05-30 21:55:00 +0300

    add parser

--- index.js
+++ index.js
@@ -2,6 +2,23 @@ import _ from 'lodash';
 
 export default function solution(content){
   // BEGIN
-  console.log(content);
+   const data = content.trim().split('\r\n');
+  const key = data[0].split(',');
+  const key1 = [...key.slice(0, 3), "Thurname", ...key.slice(-9)];
+  console.log(key1);
+  const data1 = data.slice(1).map((item) => {
+    return item.split(',');
+  });
+  
+  const dataObject = data1.map((passanger) => {
+    const passangerData = {};
+    passanger.map((item, index) => {
+      passangerData[key1[index]] = item;
+    });
+    return passangerData;
+  });
+
+  console.log(dataObject);
+
   // END
 }
